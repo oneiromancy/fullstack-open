376b2404d97b8781fc3945f611cf1089174fd45a
oneiromancy <>
2020-06-29 15:37:54 +0100

    configuring NODE_ENV for testing, development and production

--- part4/blog/utils/config.js
+++ part4/blog/utils/config.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+    process.env.NODE_ENV === 'test'
+        ? process.env.TEST_MONGODB_URI
+        : process.env.MONGODB_URI;
+
 const PORT = process.env.PORT;
 
 module.exports = { MONGODB_URI, PORT };
