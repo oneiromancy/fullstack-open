93d8ec4a524b8180d15430c88646e18faabe0ab4
oneiromancy <>
2020-06-30 21:53:58 +0100

    fixing exercises 4.8-4.14

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -15,14 +15,24 @@ blogRouter.post('/', async (req, res) => {
     const blog = new Blog({ title, author, url, likes });
     await blog.save();
 
-    return res.status(201).end();
+    return res.status(201).json(blog.toJSON());
+});
+
+blogRouter.get('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const blog = await Blog.findById(id);
+
+    if (!blog) return res.status(404).end();
+
+    res.json(blog.toJSON());
 });
 
 blogRouter.delete('/:id', async (req, res) => {
     const { id } = req.params;
-    await Blog.deleteOne({ _id: id });
+    const blog = await Blog.findByIdAndRemove(id);
 
-    return res.status(204).end();
+    return res.status(204).json(blog.toJSON());
 });
 
 blogRouter.put('/:id', async (req, res) => {
@@ -34,7 +44,7 @@ blogRouter.put('/:id', async (req, res) => {
         new: true,
     });
 
-    return res.json(blog);
+    return res.json(blog.toJSON());
 });
 
 module.exports = blogRouter;
--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -16,7 +16,7 @@ beforeEach(async () => {
 });
 
 describe('retrieving all blog posts', () => {
-    test('returns the correct number of entries in JSON format', async () => {
+    test('should return the correct number of entries in JSON format', async () => {
         const response = await api
             .get('/api/blogs')
             .expect(200)
@@ -25,7 +25,7 @@ describe('retrieving all blog posts', () => {
         expect(response.body).toHaveLength(initialBlogs.length);
     });
 
-    test('returns entries that have an id property (instead of _id)', async () => {
+    test('should return entries that have an id property', async () => {
         const response = await api.get('/api/blogs');
 
         for (let blog of response.body) {
@@ -36,26 +36,27 @@ describe('retrieving all blog posts', () => {
 
 describe('creating a blog post', () => {
     test('should increment the list of blogs by one', async () => {
-        const newBlog = new Blog({
+        const newBlog = {
             title: 'React Redux Tutorial for Beginners',
             author: 'Robin Wieruch',
             url: 'https://www.robinwieruch.de/react-redux-tutorial',
             likes: 4,
-        });
+        };
 
         await api.post('/api/blogs').send(newBlog).expect(201);
 
         const response = await api.get('/api/blogs');
+
         expect(response.body).toHaveLength(initialBlogs.length + 1);
     });
 
-    test('should make it searchable by title in list of blogs', async () => {
-        const newBlog = new Blog({
+    test('should be in the list of blogs', async () => {
+        const newBlog = {
             title: 'How to use React Memo',
             author: 'Robin Wieruch',
             url: 'https://www.robinwieruch.de/react-memo',
             likes: 4,
-        });
+        };
 
         await api.post('/api/blogs').send(newBlog).expect(201);
 
@@ -67,26 +68,28 @@ describe('creating a blog post', () => {
         expect(formattedResponse).toContain(newBlog.title);
     });
 
-    test('should default likes to zero if input is omitted', async () => {
-        const newBlog = new Blog({
+    test('should default likes to zero if the input is omitted', async () => {
+        const newBlog = {
             title: 'How to use React Testing Library Tutorial',
             author: 'Robin Wieruch',
             url: 'https://www.robinwieruch.de/react-testing-library',
-        });
+        };
 
-        await api.post('/api/blogs').send(newBlog).expect(201);
+        const postResponse = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201);
 
-        const response = await api.get('/api/blogs');
-        const lastEntry = response.body[response.body.length - 1];
+        const getResponse = await api.get(`/api/blogs/${postResponse.body.id}`);
 
-        expect(lastEntry.likes).toEqual(0);
+        expect(getResponse.body.likes).toEqual(0);
     });
 
-    test('should fail with a 400 status code if either title or url inputs are omitted', async () => {
-        const newBlog = new Blog({
+    test('should fail with a 400 status code if either the title or url inputs are omitted', async () => {
+        const newBlog = {
             author: 'Robin Wieruch',
             likes: 12,
-        });
+        };
 
         await api.post('/api/blogs').send(newBlog).expect(400);
     });
@@ -97,6 +100,8 @@ describe('deleting a blog post', () => {
         const blog = initialBlogs[initialBlogs.length - 1];
 
         await api.delete(`/api/blogs/${blog._id}`).expect(204);
+
+        await api.get(`/api/blogs/${blog._id}`).expect(404);
     });
 });
 
